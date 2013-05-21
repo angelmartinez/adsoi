3d6c315776d71619b9bfd9e1ff6ab9f920bade75
Angel Martinez <amartinez@vaynermedia.com>
2013-05-21 11:07:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adsoi::Application.config.secret_token = '1c80674c4450327af68bf322c22e76d5a008e73ada712c6388932c3d27d6fe6420725f9658f1562068ec893b772ca8b2483eb881ae8a0a4b8f50e64d22348732'
